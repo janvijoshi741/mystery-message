58941a89328ddbe32235739f8739c1336554e623
Janvi Joshi <janvij@200oksolutions.com>
2025-05-13 13:09:07 +0530

    REmoved hydration error

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -24,16 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-      
+
+      <>
         <Navbar />
         {children}
+        </>
 
-    
-      </body>
-    </html>
   );
 }
